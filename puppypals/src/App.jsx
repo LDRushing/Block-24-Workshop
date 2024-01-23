e34ac50004455d4a5006dabdea0b5f9b3bd55fbf
LDRushing <rushinglucy@yahoo.com>
2024-01-23 16:57:38 -0600

    thankstorie

--- puppypals/src/App.jsx
+++ puppypals/src/App.jsx
@@ -1,14 +1,22 @@
+import {puppyList} from './data.js'//shares information between data.js and App.jsx, kind of like when HTML is tied to a CSS and JS.
 import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+//Remember that useState returns an array. And, the array contains two elements. The first is the value that you are storing. The second is a function, which you can use to reset the value. You can deconstruct both of those values from the array, ending up with this: 
+const [puppies, setPuppies] = useState(puppyList) 
+console.log(puppies)
   return (
-      <div></div>
+      <div className="App">{
+        puppies.map((puppy) => { //displays the name of each puppy. 
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+      </div>
       
+  //strictMode, which is in my main.jsk. It's the wrap around component. It will help me find common bugs, but won't affect production build 
   
-  
-  )
+  );
 }
 
 export default App
--- puppypals/src/main.jsx
+++ puppypals/src/main.jsx
@@ -8,3 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 )
+//Don't touch this!!! This is how React will work. React DOM will inject react files in src and inject them into the DOM. App.jsx is our application, so we'll inject that. ReactDOM means to grab our root reference from the DOM and renders the code and injects it into the container. At the root, we're going to add a child root called 'the App'. The container is the div in the HTML. With React, it's a developers tool that makes frontend developement easier and puts all three files (HTML, CSS, and Javascript) into 1 file. i.e. Assets folder. Top Level of the Root is outside of the source folder (i.e. index.html, README.md, etc)
